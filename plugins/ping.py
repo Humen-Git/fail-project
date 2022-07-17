b1c382be41665b52e1aed7cf07a8b35b5357e83b
ᴍᴇ!!! <107136639+botxeditor@users.noreply.github.com>
2022-07-17 17:56:50 +0530

    Delete ping.py

--- plugins/ping.py
+++
@@ -1,38 +0,0 @@
-"""Telegram Ping / Pong Speed
-Syntax: .ping"""
-
-import time
-import random
-from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
-
-# -- Constants -- #
-ALIVE = "വെറുതെ Alive അടിച്ചു വെറുപ്പിക്കാതട ഞൻ ഇവട ജീവനോടെ ഒക്കെ തന്നെ ണ്ട് MANH ചത്തൊന്നും പോയിട്ടില്ല🥲\nഇനി ചത്താൽ update ചാനലിൽ അറിയിക്കും😵😴\n\n ⍟𝖬𝗒 𝖢𝗋𝖾𝖺𝗍𝗈𝗋: @nishn_ea\n\n⍟𝖴𝗉𝖽𝖺𝗍𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅: @VintageBotz\n\n⍟𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉: @VBotzSupport\n\n അപ്പോ ശെരി എല്ലാം പറഞ്ഞ പോലെ!!!😃🌝"
-HELP = "ദൈവമേ എന്നെ മാത്രം രക്ഷിക്കണേ...."
-REPO = "നമ്മൾ നമ്മൾ പോലുമറിയാതെ അധോലോകം ആയി മാറിക്കഴിഞ്ഞിരിക്കുന്നു ഷാജിയേട്ടാ..."
-# -- Constants End -- #
-
-
-@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
-async def check_alive(_, message):
-    await message.reply_text(ALIVE)
-
-
-@Client.on_message(filters.command("help", COMMAND_HAND_LER) & f_onw_fliter)
-async def help_me(_, message):
-    await message.reply_text(HELP)
-
-
-@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
-
-
-@Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
-async def repo(_, message):
-    await message.reply_text(REPO)
